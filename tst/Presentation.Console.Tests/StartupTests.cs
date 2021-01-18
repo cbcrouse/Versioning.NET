c79671a26226ed20c3a966e68e2560a0fbd6b992
Casey <crouse.casey@gmail.com>
2021-01-18 13:56:58 -0700

    test(Startup): Added sub command startup tests

---
+++ tst/Presentation.Console.Tests/StartupTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Presentation.Console.Tests
+{
+    public class StartupTests
+    {
+        [Fact]
+        public async Task CanStartup_IncrementVersion()
+        {
+            // Arrange
+            var args = new[] {"increment-version"};
+
+            // Act
+            await Program.Main(args);
+        }
+
+        [Fact]
+        public async Task CanStartup_IncrementVersionWithGit()
+        {
+            // Arrange
+            var args = new[] {"increment-version-with-git"};
+
+            // Act
+            await Program.Main(args);
+        }
+    }
+}
