f33b9ee28f797a42821d73ec4fbdd96e9d604601
Casey <crouse.casey@gmail.com>
2021-02-13 17:04:28 -0700

    fix(RegexPatterns): Adjusted git log commit subject pattern
    Also added tests for regex patterns.

---
+++ tst/Business.Tests/RegexPatternsTests.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Constants;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace Business.Tests
+{
+    public class RegexPatternsTests
+    {
+        [Theory]
+        [InlineData("9ed2a15 ", true)]
+        [InlineData("9ed2a15", false)]
+        public void MatchesCommitId(string commitId, bool expectedResult)
+        {
+            // Arrange
+            var sut = new Regex(RegexPatterns.GitLogCommitId);
+
+            // Act
+            bool result = sut.Match(commitId).Success;
+
+            // Act & Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("test(Test): Added ability to test", true)]
+        [InlineData("test(Different Test): test", true)]
+        [InlineData("test(Different Test) : test", true)]
+        [InlineData("test(Different Test): ", false)]
+        [InlineData("(Different Test): test", false)]
+        public void MatchesCommitSubject(string commitId, bool expectedResult)
+        {
+            // Arrange
+            var sut = new Regex(RegexPatterns.GitLogCommitSubject);
+
+            // Act
+            bool result = sut.IsMatch(commitId);
+
+            // Act & Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
